cc362d4f4dfeca294119e96c33682305942b7530
sh4062 <mx0666@163.com>
2018-04-06 21:30:50 +0800

    Update

--- 7_Reverse Integer.cpp
+++ 7_Reverse Integer.cpp
@@ -21,3 +21,25 @@ public:
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     int reverse(int x) {
+//         int flag = 1;
+//         if (x == INT_MIN) {
+//             return 0;
+//         }
+//         if(x<0) {flag = -1;x= -x;}
+//         long int rev = 0;
+//         while(x){
+//             rev = rev*10+x%10;
+//             if (rev > INT_MAX) {
+//                 return 0;
+//             }
+//             x=x/10;
+//         }
+//         return flag*rev;
+        
+        
+//     }
+// };
