ad5e706c25db8349abd7d89e995f62020e3c74c6
sh4062 <mx0666@163.com>
2020-04-03 11:40:23 +0800

    1078

---
+++ 1078.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    vector<string> split(const string& str, const string& delim) {  
+	vector<string> res;  
+	if("" == str) return res;  
+	//先将要切割的字符串从string类型转换为char*类型  
+	char * strs = new char[str.length() + 1] ; //不要忘了  
+	strcpy(strs, str.c_str());   
+
+	char * d = new char[delim.length() + 1];  
+	strcpy(d, delim.c_str());  
+
+	char *p = strtok(strs, d);  
+	while(p) {  
+		string s = p; //分割得到的字符串转换为string类型  
+		res.push_back(s); //存入结果数组  
+		p = strtok(NULL, d);  
+	}  
+
+	return res;  
+} 
+public:
+    vector<string> findOcurrences(string text, string first, string second) {
+        vector<string> tmp = split(text," ");
+        vector<string> res;
+        int c = 0;
+        for(int i = 0;i<tmp.size()-2;i++){
+            if(tmp[i].compare(first)==0&&tmp[i+1].compare(second)==0){
+                res.push_back(tmp[i+2]);
+            }
+        }
+        return res;
+        
+    }
+};
