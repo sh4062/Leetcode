cddf0eb1e118c7deb3c1ef015b9e49aabbd4a98c
sh4062 <mx0666@163.com>
2020-06-04 14:16:04 +0800

    naive BST kth

---
+++ 230.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<int> res;
+public:
+    void inorder(TreeNode* root){
+        if(root==nullptr)return;
+        
+        if(root->left)inorder(root->left);
+        res.push_back(root->val);
+        if(root->right)inorder(root->right);
+        
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root);
+        return res[k-1];
+        
+    }
+};
