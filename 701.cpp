0ebdc171f6a0bde866cee26162379b0a3c76b470
sh4062 <mx0666@163.com>
2020-11-24 14:19:01 +0800

    Create 701.cpp

---
+++ 701.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(!root){
+            root=new TreeNode(val);
+        } else
+        if(!root->left&&root->val>val){
+            root->left=new TreeNode(val);
+            
+        }else if(!root->right&&root->val<val){
+            root->right=new TreeNode(val);
+            
+        }else if(val>root->val){
+             insertIntoBST(root->right,val);
+        }else if(val<root->val){
+             insertIntoBST(root->left,val);
+        }
+        
+        return root;
+    }
+};
