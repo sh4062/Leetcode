8af6ef58f451fa68b772b13964471591834e36a9
sh4062 <mx0666@163.com>
2021-02-06 14:22:24 +0800

    Create 165.py

---
+++ 165.py
@@ -0,0 +1,28 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1=version1.split('.')
+        v2=version2.split('.')
+        l = min(len(v1),len(v2))
+        i = 0
+        for i in range(l):
+            if int(v1[i])>int(v2[i]):
+                return 1
+            if int(v1[i])<int(v2[i]):
+                return -1
+        i+=1
+        print(i)
+        while i <len(v1):
+            if int(v1[i])!=0:
+                return 1
+            i+=1
+                
+
+
+        while i <len(v2):
+            if int(v2[i])!=0:
+                return -1
+            i+=1
+
+
+        return 0
+
