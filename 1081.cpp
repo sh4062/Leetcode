3d6316a724f9640a42f45fd7ebdc0a571adf479e
sh4062 <mx0666@163.com>
2021-02-09 17:24:33 +0800

    Create 1081.cpp

---
+++ 1081.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+       string res;
+        vector<char>st;
+        vector<int> hash(26,0);
+        int resl=0;
+        for(auto i:s){
+            hash[i-'a']++;
+            if(hash[i-'a']==1)
+            resl++;
+        }
+        //
+        for(int i = 0;i<s.size();i++){
+            while(st.size()>0&&s[i]<st[st.size()-1]&&hash[st[st.size()-1]-'a'] >1&&find(st.begin(),st.end(),s[i])==st.end()){
+                hash[st[st.size()-1]-'a']--;
+                cout<<st[st.size()-1]<<endl;
+                st.pop_back();
+            }
+            if(find(st.begin(),st.end(),s[i])==st.end()){
+
+            // hash[s[i]-'a']--;
+            st.push_back(s[i]);
+            }else{
+                hash[s[i]-'a']--;
+            }
+        }
+        //cout<<resl;
+        res=string(st.begin(),st.end());
+        
+
+
+        return res.substr(0,resl);
+    }
+};
