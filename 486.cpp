9bc133d08754d7406cd426c1849bf2d486b1ee4f
sh4062 <mx0666@163.com>
2021-02-11 19:07:59 +0800

    Create 486.cpp

---
+++ 486.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),0));
+        for(int i = 0;i<nums.size();i++){
+            dp[i][i]=nums[i];
+        }
+        for (int i = nums.size(); i >= 0; i--) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][nums.size() - 1] >= 0;
+        //return true;
+
+    }
+};
