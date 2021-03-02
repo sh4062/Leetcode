b69314e35450b605681f28f1c0f191bf80169083
sh4062 <mx0666@163.com>
2021-03-02 22:20:06 +0800

    Create 309.cpp

---
+++ 309.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()==0)return 0;
+         int n = prices.size();
+        //n天 有没有股票0/1
+        int dp[n][2];
+        memset(dp,0,sizeof(dp));
+        dp[0][0]=-prices[0];
+        dp[0][0]=-prices[0];
+        //dp[0][2][0]=-prices[0];
+        for(int i = 1;i<n;i++){
+                for(int k : {0,1}){
+                    //cout<<k;
+                    if(k==0){
+                    if(i>2)
+                    dp[i][k]=max(dp[i-1][0],dp[i-2][1]-prices[i]);
+                    else{
+                        dp[i][k]=max(dp[i-1][0],-prices[i]);
+                    }
+                    }else{
+                   
+                    dp[i][k]=max(dp[i-1][1],dp[i-1][0]+prices[i]);
+                    
+                    }
+
+                }
+            }
+        
+
+
+
+        return dp[n-1][1];
+
+    }
+};
