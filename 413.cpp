696763d2450235bed99ccdf3306e407a3b2a38c3
sh4062 <mx0666@163.com>
2021-04-21 21:31:50 +0800

    Create 413.cpp

---
+++ 413.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        vector<int>dp(nums.size(),0);
+        for(int i = 2;i<nums.size();i++){
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
+                dp[i]=max(dp[i],dp[i-1]+1);
+            }
+            
+        }
+        int res = accumulate(dp.begin(),dp.end(),0);
+        return res;
+
+    }
+};
