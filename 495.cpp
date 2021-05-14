adcbabf899a41614b48f4a14a7c21a2e9eee2c50
sh4062 <mx0666@163.com>
2021-05-14 23:08:54 +0800

    Create 495.cpp

---
+++ 495.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        if(!timeSeries.size()  || timeSeries.size() == 0){
+            return 0;
+        }
+        int sum = 0;
+        for(int i = 1; i < timeSeries.size(); i++){
+            if(timeSeries[i]-timeSeries[i-1] >= duration){
+                sum +=duration;
+            }else{
+                sum += timeSeries[i]-timeSeries[i-1];
+            }
+        }
+        return sum+duration;
+
+    }
+};
