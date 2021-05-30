eeb4e411b38163d0dae3cc57293d7c5266b66d49
sh4062 <mx0666@163.com>
2021-05-30 21:41:52 +0800

    Create 231.cpp

---
+++ 231.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n==0)return false;
+        if(n==1)return true;
+        if(n<0)return false;
+        while(n>1){
+            if(n%2!=0){
+                return false;
+            }
+            n>>=1;
+        }
+        return true;
+
+    }
+};
