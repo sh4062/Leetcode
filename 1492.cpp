8a65fe0cf2745a6a2bd6f74812dbece55449c030
sh4062 <mx0666@163.com>
2021-06-02 17:21:46 +0800

    Create 1492.cpp

---
+++ 1492.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        for(int i = 1, cnt = 0; i <= n; ++i) {
+            if(n % i == 0) ++cnt;
+            if(cnt == k) return i;
+        }
+        return -1;
+
+    }
+};
