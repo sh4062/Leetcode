7e3f01e0217d51ee1ac287f16efda57f70594d32
sh4062 <mx0666@163.com>
2021-06-09 23:33:33 +0800

    Create 445.cpp

---
+++ 445.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1,s2;
+        while(l1!=nullptr){
+            s1.push(l1->val);
+            l1=l1->next;
+        }
+        while(l2!=nullptr){
+            s2.push(l2->val);
+            l2=l2->next;
+        }
+        
+        int curr=0;
+        int carry=0;
+        
+        ListNode* ans=nullptr;
+
+        while(!s1.empty()|!s2.empty()|carry!=0){
+            int a=s1.empty()?0:s1.top();
+            int b=s2.empty()?0:s2.top();
+            if(!s1.empty())    s1.pop();
+            if(!s2.empty())    s2.pop();
+
+            curr=a+b+carry;
+            carry=curr/10;
+            //curr=(a+b+carry)%10;//注意这里的carry被改变了，所以会错
+            curr=curr%10;
+
+            ListNode* currnode=new ListNode(curr);
+            currnode->next=ans;
+            ans=currnode;
+        }
+        return ans;
+
+    }
+};
